8ddceab22dc2f3563b8bee4aa7058a9b42d2d211
xiaojuntao <find_onepiece@163.com>
2021-12-24 23:49:38 +0800

    反射

--- src/go_code/reflect/reflect01/main/main.go
+++ src/go_code/reflect/reflect01/main/main.go
@@ -6,6 +6,7 @@ import (
 	"reflect"
 )
 
+// 基本数据类型反射
 func reflecttest01(b interface{}) {
 	// 通过反射获取到变量的 Type 信息
 	rTyp := reflect.TypeOf(b)
@@ -16,20 +17,68 @@ func reflecttest01(b interface{}) {
 	rVal := reflect.ValueOf(b)
 	fmt.Printf("rVal类型： %T, 值： %v \n", rVal, rVal) // rVal类型： reflect.Value, 值： 100
 	fmt.Println("rVal Kind : ", rVal.Kind())       // int
-	n2 := 10 + rVal.Int()
-	fmt.Println("原始值： ", rVal.Int()) // 原始值：  100
-	fmt.Println("n2 = ", n2)         // 110
+	// n2 := 10 + rVal.Int()
+	// fmt.Println("原始值： ", rVal.Int()) // 原始值：  100
+	// fmt.Println("n2 = ", n2)         // 110
+
+	// rVal.SetInt(20) // panic: reflect: reflect.Value.SetInt using unaddressable value
+	rVal.Elem().SetInt(20)
 
 	// 将 rVal 转成 interface{}
 	iV := rVal.Interface()
 	fmt.Printf("iV 类型： %T， 值： %v \n", iV, iV) // iV 类型： int， 值： 100
 	// 断言
-	num2 := iV.(int)
-	fmt.Printf("num2 类型： %T， 值： %v \n", num2, num2) // num2 类型： int， 值： 100
+	num2, ok := iV.(int)
+	if ok {
+		fmt.Printf("num2 类型： %T， 值： %v \n", num2, num2) // num2 类型： int， 值： 100
+	} else {
+		fmt.Println("断言失败XXXX")
+	}
+}
+
+// 结构体类型反射
+func reflecttest02(b interface{}) {
+	// 通过反射获取到变量的 Type 信息
+	rTyp := reflect.TypeOf(b)
+	fmt.Printf("rTyp类型： %T, 值： %v \n", rTyp, rTyp) // rTyp类型： *reflect.rtype, 值： main.Stu
+	fmt.Println("rTyp.Name", rTyp.Name())          // rTyp.Name Stu
+
+	// 获取 reflect.Value
+	rVal := reflect.ValueOf(b)
+	fmt.Printf("rVal类型： %T, 值： %v \n", rVal, rVal) // rVal类型： reflect.Value, 值： {tom 20}
+	fmt.Println("rVal Kind : ", rVal.Kind())       // rVal Kind :  struct
+
+	// 将 rVal 转成 interface{}
+	iV := rVal.Interface()
+	fmt.Printf("iV 类型： %T， 值： %v \n", iV, iV) // iV 类型： main.Stu， 值： {tom 20}
+	// 虽然 iv的 值是 {tom 20}， 但是仍然无法直接取出 Name,
+	// 反射是运行时，不是编译时，仍需要断言
+	// fmt.Printf("name = %v \n", iV.Name) // iV.Name undefined (type interface {} is interface with no methods)
+	// 带检测的断言
+	_stu, ok := iV.(Stu)
+	if ok {
+		fmt.Printf("_stu 类型： %T， 值： %v \n", _stu, _stu) // _stu 类型： main.Stu， 值： {tom 20}
+		fmt.Printf("name = %v \n", _stu.Name)
+	} else {
+		fmt.Println("断言失败")
+	}
+}
+
+type Stu struct {
+	Name string
+	Age  int
 }
 
 func main() {
 	// 基本数据类型 - interface{} - reflect.ValueOf()
 	var num int = 100
-	reflecttest01(num)
+	reflecttest01(&num)
+	fmt.Println("num ===== ", num)
+
+	// 结构体数据类型 - interface{} - reflect.ValueOf()
+	var stu Stu = Stu{
+		Name: "tom",
+		Age:  20,
+	}
+	reflecttest02(stu)
 }
---
+++ src/go_code/reflect/reflect03/main/main.go
@@ -0,0 +1,78 @@
+// 结构体 反射 案例
+// 反射可以用来开发底层框架
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// 这就需要通过 tag 来转化大写的字段，同时又保证内部调用的时候可以暴露变量
+// 反射原理
+type People struct {
+	Name   string  `json:"name01"`
+	Age    int     `json:"age"`
+	Height float64 `json:"height"`
+	Action string
+}
+
+// 给结构体绑定方法
+
+func (p People) Print() {
+	fmt.Println("print() = ", p)
+}
+
+func (p People) GetSum(n1, n2 int) int {
+	return n1 + n2
+}
+
+func (p People) Set(name string, age int, height float64, action string) {
+	p.Name = name
+	p.Age = age
+	p.Height = height
+	p.Action = action
+}
+
+func TestStruct(a interface{}) {
+	typ := reflect.TypeOf(a)
+	val := reflect.ValueOf(a)
+	kd := val.Kind()
+	if kd != reflect.Struct {
+		fmt.Println("except struct, but its not")
+		return
+	}
+	// 结构体字段
+	num := val.NumField()
+	fmt.Printf("struct has %v fields \n", num)
+	for i := 0; i < num; i++ {
+		fmt.Printf("第%v个字段：%v \n", i+1, val.Field(i))
+		tagVal := typ.Field(i).Tag.Get("json")
+		if tagVal != "" {
+			fmt.Printf("第%v个字段, tag ： %v \n", i+1, tagVal)
+		}
+	}
+	// 结构体方法
+	numOfMethod := val.NumMethod()
+	fmt.Printf("struct 有 %v 个方法 \n", numOfMethod)
+
+	// 内部方法名顺序，按照函数名的 ascii 码排序的
+	val.Method(1).Call(nil)
+
+	var params []reflect.Value
+	params = append(params, reflect.ValueOf(10))
+	params = append(params, reflect.ValueOf(40))
+	res := val.Method(0).Call(params)
+	fmt.Println("res = ", res[0].Int())
+}
+
+func main() {
+	// struct
+
+	p1 := People{
+		Name:   "tom",
+		Age:    18,
+		Height: 178.5,
+		Action: "衣食住行",
+	}
+	TestStruct(p1)
+}
